41b005b9134c9923f689ce1243aa3961f5215e85
JasonV2323 <124284381+JasonV2323@users.noreply.github.com>
2023-02-06 11:38:25 -0500

    This is version 2 of my Hello World Program

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,8 @@
-#This is version 1 of my hello world program
-print ("Hello my name is Jason")
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/Jason/Downloads/helloWorld.py ===============
+Hello my name is Jason
+>>> 
+================ RESTART: C:/Users/Jason/Downloads/helloWorld.py ===============
+Hello my name is Jason Veres
